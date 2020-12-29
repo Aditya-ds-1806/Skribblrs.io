7d19bc85295b32611cf10cfa3f04eb2122824f9d
Aditya DS <adithya18062000@gmail.com>
2020-12-30 03:06:20 +0530

    basic scoreboard and chat implementation

--- public/js/control.js
+++ public/js/control.js
@@ -6,6 +6,22 @@ var copyBtn = document.querySelector("#copy");
 socket.on("joinRoom", putPlayer);
 socket.on("otherPlayers", players => players.forEach(player => putPlayer(player)));
 socket.on("disconnection", player => document.querySelector(`#${player.id}`).remove());
+socket.on("startGame", showCanvasArea);
+socket.on("getPlayers", players => createScoreCard(players));
+socket.on("message", ({ sender, message }) => {
+    var p = document.createElement("p");
+    var chat = document.createTextNode(`${sender}: ${message}`);
+    p.classList.add("p-2", "mb-0");
+    p.append(chat);
+    document.querySelector(".messages").appendChild(p);
+});
+
+document.querySelector("#sendMessage").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const message = this.firstElementChild.value;
+    this.firstElementChild.value = "";
+    socket.emit("message", { sender: socket.id, message: message });
+})
 
 if (searchParams.has("id")) {
     // player
@@ -13,6 +29,7 @@ if (searchParams.has("id")) {
     document.querySelector("#createRoom").classList.add('disabled');
     document.querySelector("#rounds").setAttribute('disabled', true);
     document.querySelector("#time").setAttribute('disabled', true);
+    document.querySelector("#startGame").setAttribute('disabled', true);
 
     socket.on("settingsUpdate", data => {
         document.querySelector("#rounds").value = data.rounds;
@@ -49,6 +66,41 @@ function putPlayer(player) {
     document.querySelector("#playersDiv").appendChild(div);
 }
 
+function createScoreCard(players) {
+    players.forEach(player => {
+        var div = document.createElement('div');
+        var avatar = document.createElement('div');
+        var details = document.createElement('div');
+        var img = document.createElement('img');
+        var p1 = document.createElement('p');
+        var p2 = document.createElement('p');
+        var name = document.createTextNode(player.name);
+        var score = document.createTextNode("Score: 100");
+
+        img.src = player.avatar;
+        img.classList.add("img-fluid");
+        div.classList.add("row", "justify-content-end", "bg-white", "py-1");
+        avatar.classList.add("col-4");
+        details.classList.add("col-6", "text-center", "my-auto");
+        p1.classList.add("mb-0");
+        p2.classList.add("mb-0");
+        div.append(details, avatar);
+        avatar.append(img);
+        details.append(p1, p2);
+        p1.append(name);
+        p2.append(score);
+        document.querySelector(".players").append(div);
+    });
+}
+
+function showCanvasArea() {
+    var script = document.createElement('script');
+    document.querySelector("#settings").classList.add("d-none");
+    document.querySelector("#gameZone").classList.remove("d-none");
+    script.src = "js/canvas.js";
+    document.body.append(script);
+}
+
 copyBtn.addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector("#gameLink").select();
@@ -78,9 +130,7 @@ document.querySelector("#playGame").addEventListener("click", function () {
 });
 
 document.querySelector("#startGame").addEventListener("click", function () {
-    document.querySelector("#settings").classList.add("d-none");
-    document.querySelector("#gameZone").classList.remove("d-none");
-    var script = document.createElement('script');
-    script.src = "js/canvas.js";
-    document.body.append(script);
+    showCanvasArea();
+    socket.emit("startGame");
+    socket.emit("getPlayers");
 });
