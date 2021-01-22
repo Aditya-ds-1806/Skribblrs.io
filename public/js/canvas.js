939e697886850e0477ef9869980bae87620578ac
Aditya DS <adithya18062000@gmail.com>
2021-01-22 20:52:41 +0530

    Fixes #13

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -6,22 +6,79 @@ const pad = new Sketchpad(canvas, {
     },
     aspectRatio: 5 / 8
 });
+const current = {
+    lineColor: "#000"
+};
 pad.setReadOnly(true);
 
 colors.forEach(color => {
-    color.addEventListener('click', e => pad.setLineColor(e.target.className.split(' ')[1], false));
+    color.addEventListener('click', e => {
+        current.lineColor = e.target.className.split(' ')[1];
+        pad.setLineColor(current.lineColor);
+    }, false)
 })
 
 window.addEventListener('resize', () => pad.resize(canvas.offsetWidth));
+canvas.addEventListener('mousedown', onMouseDown);
+canvas.addEventListener('mouseup', throttle(onMouseUp, 10));
 canvas.addEventListener('mousemove', throttle(onMouseMove, 10));
 
 socket.on('clearCanvas', () => pad.clear());
-socket.on('drawing', data => pad.loadJSON(data));
+socket.on('drawing', ({ start, end, lineColor }) => {
+    const { width: w, height: h } = pad.getCanvasSize();
+    start.x *= w;
+    start.y *= h;
+    end.x *= w;
+    end.y *= h;
+    current.lineColor = lineColor;
+    pad.setLineColor(current.lineColor);
+    pad.drawLine(start, end);
+});
 socket.on("disableCanvas", () => pad.setReadOnly(true));
+
+function onMouseDown(e) {
+    if (!pad.sketching) return;
+    const rect = canvas.getBoundingClientRect();
+    const { width: w, height: h } = pad.getCanvasSize();
+    current.x = (e.clientX - rect.left) / w;
+    current.y = (e.clientY - rect.top) / h;
+}
+
+function onMouseUp(e) {
+    if (pad.readOnly) return;
+    const rect = canvas.getBoundingClientRect();
+    const { width: w, height: h } = pad.getCanvasSize();
+    console.log("mouseup");
+    socket.emit('drawing', {
+        start: {
+            x: current.x,
+            y: current.y,
+        },
+        end: {
+            x: (e.clientX - rect.left) / w,
+            y: (e.clientY - rect.top) / h
+        },
+        lineColor: current.lineColor
+    });
+}
 
 function onMouseMove(e) {
     if (!pad.sketching) return;
-    socket.emit('drawing', pad.toJSON());
+    const { width: w, height: h } = pad.getCanvasSize();
+    const rect = canvas.getBoundingClientRect();
+    socket.emit('drawing', {
+        start: {
+            x: current.x,
+            y: current.y,
+        },
+        end: {
+            x: (e.clientX - rect.left) / w,
+            y: (e.clientY - rect.top) / h
+        },
+        lineColor: current.lineColor
+    });
+    current.x = (e.clientX - rect.left) / w;
+    current.y = (e.clientY - rect.top) / h;
 }
 
 // limit the number of events per second
